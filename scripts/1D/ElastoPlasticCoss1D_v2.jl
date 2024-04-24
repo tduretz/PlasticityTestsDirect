0fe1bc734ca695e551ff0f344267e824fcb652d5
tduretz <duretz@uni-frankfurt.de>
2024-04-24 15:01:02 +0200

    add Coss

--- scripts/1D/ElastoPlasticCoss1D_v2.jl
+++ scripts/1D/ElastoPlasticCoss1D_v2.jl
@@ -1,13 +1,15 @@
 using Plots, SparseArrays, Symbolics, SparseDiffTools, Printf, CSV, DataFrames, IfElse
 import LinearAlgebra: norm
 
+# Neumann off
+
 # with inertia
 
 function NeumannVerticalVelocity(σ_BC, VyS, G, K, τyy0, P0, Δy, Δt)
     return (2.0 * G .* VyS .* Δt + 3.0 * K .* VyS .* Δt + 2.0 * P0 .* Δy + 2.0 * Δy .* σ_BC.yy - 2.0 * Δy .* τyy0) ./ (Δt .* (2.0 * G + 3.0 * K))
 end
 
-function LocalStress(VxN, VxS, VyN, VyS, P, P0, τxx0, τyy0, τzz0, τxy0, G, K, ϕ, ψ, c, ηvp, Δy, Δt)
+function LocalStress(VxN, VxS, VyN, VyS, ω̇zN, ω̇zS, P, P0, τxx0, τyy0, τzz0, τxy0, Rz0, myz0, G, K, lc, ϕ, ψ, c, ηvp, Δy, Δt)
     
     # Total strain rate
     ε̇xxt  = 0.0
@@ -15,6 +17,9 @@ function LocalStress(VxN, VxS, VyN, VyS, P, P0, τxx0, τyy0, τzz0, τxy0, G, K
     ε̇zzt  = 1/2*(ε̇xxt + ε̇yyt)
     ε̇xyt  = 1/2*( VxN -  VxS)/Δy
     divV  = ε̇xxt + ε̇yyt + ε̇zzt
+    ω̇z    = 0.5*(ω̇zN + ω̇zS)
+    Ẇz    = 1/2*( VxN -  VxS)/Δy + ω̇z
+    κ̇yz   = (ω̇zN - ω̇zS)/Δy
 
     # Deviatoric strain rate
     ε̇xx   = ε̇xxt - 1/3*divV
@@ -23,13 +28,15 @@ function LocalStress(VxN, VxS, VyN, VyS, P, P0, τxx0, τyy0, τzz0, τxy0, G, K
     ε̇xy   = ε̇xyt
 
     # Deviatoric stress
-    τxx   = 2*G*Δt*ε̇xx + τxx0
-    τyy   = 2*G*Δt*ε̇yy + τyy0
-    τzz   = 2*G*Δt*ε̇zz + τzz0
-    τxy   = 2*G*Δt*ε̇xy + τxy0
-    
+    τxx   =      2*G*Δt*ε̇xx + τxx0
+    τyy   =      2*G*Δt*ε̇yy + τyy0
+    τzz   =      2*G*Δt*ε̇zz + τzz0
+    τxy   =      2*G*Δt*ε̇xy + τxy0
+    Rz    =     -2*G*Δt*Ẇz  + Rz0 
+    myz   = lc^2*2*G*Δt*κ̇yz + myz0
+
     # Yield function
-    τII   = sqrt(1/2*(τxx^2 + τyy^2 + τzz^2) + τxy^2)
+    τII   = sqrt(1/2*(τxx^2 + τyy^2 + τzz^2 + (myz/lc)^2) + τxy^2 + Rz^2)
     Pc    = P
     f     = τII - c*cos(ϕ) - Pc*sin(ϕ)
 
@@ -42,100 +49,123 @@ function LocalStress(VxN, VxS, VyN, VyS, P, P0, τxx0, τyy0, τzz0, τxy0, G, K
     ε̇yyp  = λ̇*(τyy)/2/τII
     ε̇zzp  = λ̇*(τzz)/2/τII
     ε̇xyp  = λ̇*(τxy)/1/τII
-    τxx   = 2*G*Δt*(ε̇xx - ε̇xxp  ) + τxx0
-    τyy   = 2*G*Δt*(ε̇yy - ε̇yyp  ) + τyy0
-    τzz   = 2*G*Δt*(ε̇zz - ε̇zzp  ) + τzz0
-    τxy   = 2*G*Δt*(ε̇xy - ε̇xyp/2) + τxy0
+    ẇzp   = λ̇*( Rz/2/τII) 
+    κ̇yzp  = λ̇*(myz/2/τII)/lc^2
+    τxx   =      2*G*Δt*(ε̇xx - ε̇xxp  ) + τxx0
+    τyy   =      2*G*Δt*(ε̇yy - ε̇yyp  ) + τyy0
+    τzz   =      2*G*Δt*(ε̇zz - ε̇zzp  ) + τzz0
+    τxy   =      2*G*Δt*(ε̇xy - ε̇xyp/2) + τxy0
+    Rz    =     -2*G*Δt*(Ẇz + ẇzp)   + Rz0 
+    myz   = lc^2*2*G*Δt*(κ̇yz - κ̇yzp) + myz0
     Pc    = P + λ̇*K*Δt*sin(ψ)
-    τII   = sqrt(1/2*(τxx^2 + τyy^2 + τzz^2) + τxy^2)
+    τII   = sqrt(1/2*(τxx^2 + τyy^2 + τzz^2 + (myz/lc)^2) + τxy^2 + Rz^2)
     fc    = τII - c*cos(ϕ) - Pc*sin(ϕ) - ηvp*λ̇
 
-    return τxx, τyy, τzz, τxy, Pc, fc, pl, ε̇yyt
+    return τxx, τyy, τzz, τxy, Pc, Rz, myz, fc, pl, ε̇yyt
 end
 
-function ComputeStress!(τxx, τyy, τzz, τxy, Pc, Fc, Pl, εyyt,  x, P, P0, τxx0, τyy0, τzz0, τxy0, V_BC, σ_BC, rheo, iV, NumV, Δy, Δt )
+function ComputeStress!(τxx, τyy, τzz, τxy, Pc, Rz, Myz, Fc, Pl, εyyt,  x, P, P0, τxx0, τyy0, τzz0, τxy0, Rz0, myz0, V_BC, σ_BC, rheo, ind, NumV, Δy, Δt )
 
     # Loop over all stress nodes
     for i=1:length(NumV.x)+1
-        iVx = i 
-        iVy = iV.x+i 
-        iP  = iV.y+i
+        ix  = i 
+        iy  = ind.x+i 
+        iP  = ind.y+i
+        iω̇z = ind.p+i
         VxS, VxN = 0., 0. 
         VyS, VyN = 0., 0. 
         p   = x[iP] 
         if i==1 # Special case for bottom (South) vertex
-            VxS = 2*V_BC.x.S - x[iVx]
-            VxN = x[iVx]
-            VyS = 2*V_BC.y.S - x[iVy]
-            VyN = x[iVy]
+            VxS = 2*V_BC.x.S - x[ix]
+            VxN = x[ix]
+            VyS = 2*V_BC.y.S - x[iy]
+            VyN = x[iy]
+            ω̇zS = x[iω̇z]
+            ω̇zN = x[iω̇z]
         elseif i==length(NumV.x)+1 # Special case for top (North) vertex
-            VxS = x[iVx-1]
-            VxN = 2*V_BC.x.N - x[iVx-1]
-            VyS = x[iVy-1]
-            VyN = NeumannVerticalVelocity(σ_BC, x[iVy-1], rheo.G[end], rheo.K[end], τyy0[end], P0[end], Δy, Δt)
+            VxS = x[ix-1]
+            VxN = 2*V_BC.x.N - x[ix-1]
+            VyS = x[iy-1]
+            # VyN = 2*V_BC.y.N - x[iy-1] 
+            VyN = NeumannVerticalVelocity(σ_BC, x[iy-1], rheo.G[end], rheo.K[end], τyy0[end], P0[end], Δy, Δt)
+            ω̇zS = x[iω̇z-1]
+            ω̇zN = x[iω̇z-1]
         else # General case
-            VxS = x[iVx-1]
-            VxN = x[iVx]
-            VyS = x[iVy-1]
-            VyN = x[iVy]
+            VxS = x[ix-1]
+            VxN = x[ix]
+            VyS = x[iy-1]
+            VyN = x[iy]
+            ω̇zS = x[iω̇z-1]
+            ω̇zN = x[iω̇z]
         end
-        # Compute stress and corrcted pressure 
-        τ11, τ22, τ33, τ21, pc, fc, pl, ε̇yyt = LocalStress( VxN, VxS, VyN, VyS, p, P0[i], τxx0[i], τyy0[i], τzz0[i], τxy0[i], rheo.G[i], rheo.K[i], rheo.ϕ[i], rheo.ψ[i], rheo.c[i], rheo.ηvp[i], Δy, Δt )
+        # Compute stress and corrected pressure 
+        τ11, τ22, τ33, τ21, pc, rz, myz, fc, pl, ε̇yyt = LocalStress( VxN, VxS, VyN, VyS, ω̇zN, ω̇zS, p, P0[i], τxx0[i], τyy0[i], τzz0[i], τxy0[i], Rz0[i], myz0[i], rheo.G[i], rheo.K[i], rheo.lc[i], rheo.ϕ[i], rheo.ψ[i], rheo.c[i], rheo.ηvp[i], Δy, Δt )
         τxx[i]   = τ11
         τyy[i]   = τ22
         τzz[i]   = τ33
         τxy[i]   = τ21
         Pc[i]    = pc
+        Rz[i]    = rz
+        Myz[i]   = myz
         Fc[i]    = fc
         Pl[i]    = pl 
         εyyt[i] += ε̇yyt*Δt
     end
 end
 
-function Res!(F, x, P, P0, τxx0, τyy0, τzz0, τxy0, ρ, Vx0, Vy0, V_BC, σ_BC, rheo, iV, NumV, Δy, Δt )
+function Res!(F, x, P, P0, τxx0, τyy0, τzz0, τxy0, Rz0, myz0, ρ, Vx0, Vy0, V_BC, σ_BC, rheo, ind, NumV, Δy, Δt )
 
     # Loop over all velocity nodes
     for i=1:length(NumV.x)
-        iVx = i
-        iVy = iV.x+i
-        iP  = iV.y+i
-        VxC, VxS, VxN = x[iVx], 0., 0.
-        VyC, VyS, VyN = x[iVy], 0., 0.
+        ix   = i
+        iy   = ind.x+i
+        iP   = ind.y+i
+        iω̇z  = ind.p+i
+        VxC, VxS, VxN = x[ix],  0., 0.
+        VyC, VyS, VyN = x[iy],  0., 0.
+        ω̇zC, ω̇zS, ω̇zN = x[iω̇z], 0., 0.
         PS     = x[iP]
         PN     = x[iP+1]
         if i==1
             VxS = 2*V_BC.x.S - VxC
             VyS = 2*V_BC.y.S - VyC
+            ω̇zS = ω̇zC
         else
-            VxS = x[iVx-1]
-            VyS = x[iVy-1]
+            VxS = x[ix-1]
+            VyS = x[iy-1]
+            ω̇zS = x[iω̇z-1]
         end
-        if i==iV.x
+        if i==ind.x
             VxN = 2*V_BC.x.N - VxC
+            # VyN = 2*V_BC.y.N - VyC 
             VyN = NeumannVerticalVelocity(σ_BC, VyC, rheo.G[end], rheo.K[end], τyy0[end], P0[end], Δy, Δt)
+            ω̇zN = ω̇zC
         else
-            VxN = x[iVx+1]
-            VyN = x[iVy+1]
+            VxN = x[ix+1]
+            VyN = x[iy+1]
+            ω̇zN = x[iω̇z+1]
         end
-        τxxN, τyyN, τzzN, τxyN, PN = LocalStress( VxN, VxC, VyN, VyC, PN, P0[i+1], τxx0[i+1], τyy0[i+1], τzz0[i+1], τxy0[i+1], rheo.G[i+1], rheo.K[i+1], rheo.ϕ[i+1], rheo.ψ[i+1], rheo.c[i+1], rheo.ηvp[i+1], Δy, Δt )
-        τxxS, τyyS, τzzS, τxyS, PS = LocalStress( VxC, VxS, VyC, VyS, PS, P0[i],   τxx0[i],   τyy0[i],   τzz0[i],   τxy0[i],   rheo.G[i],   rheo.K[i],   rheo.ϕ[i],   rheo.ψ[i],   rheo.c[i],   rheo.ηvp[i],   Δy, Δt )
-        F[iVx] = (τxyN - τxyS)/Δy                - ρ[i]*(VxC-Vx0[i])/Δt
-        F[iVy] = (τyyN - τyyS)/Δy - (PN - PS)/Δy - ρ[i]*(VyC-Vy0[i])/Δt
+        τxxN, τyyN, τzzN, τxyN, PN, RzN, myzN = LocalStress( VxN, VxC, VyN, VyC, ω̇zN, ω̇zC, PN, P0[i+1], τxx0[i+1], τyy0[i+1], τzz0[i+1], τxy0[i+1], Rz0[i+1], myz0[i+1], rheo.G[i+1], rheo.K[i+1],rheo.lc[i+1], rheo.ϕ[i+1], rheo.ψ[i+1], rheo.c[i+1], rheo.ηvp[i+1], Δy, Δt )
+        τxxS, τyyS, τzzS, τxyS, PS, RzS, myzS = LocalStress( VxC, VxS, VyC, VyS, ω̇zC, ω̇zS, PS, P0[i],   τxx0[i],   τyy0[i],   τzz0[i],   τxy0[i],   Rz0[i],   myz0[i],   rheo.G[i],   rheo.K[i],  rheo.lc[i],   rheo.ϕ[i],   rheo.ψ[i],   rheo.c[i],   rheo.ηvp[i],   Δy, Δt )
+        F[ix]  = (τxyN - τxyS)/Δy - (RzN - RzS)/Δy - ρ[i]*(VxC-Vx0[i])/Δt
+        F[iy]  = (τyyN - τyyS)/Δy - (PN  - PS )/Δy - ρ[i]*(VyC-Vy0[i])/Δt
+        F[iω̇z] = (myzN - myzS)/Δy + 2*(RzN + RzS)/2
     end
     # Loop over all stress nodes
     for i=1:length(NumV.x)+1
-        iP  = iV.y+i
-        iVy = iV.x+i 
+        iP  = ind.y+i
+        iy  = ind.x+i 
         VyS, VyN = 0., 0. 
         if i==1
-            VyS = 2*V_BC.y.S - x[iVy]
-            VyN = x[iVy]
+            VyS = 2*V_BC.y.S - x[iy]
+            VyN = x[iy]
         elseif i==length(NumV.x)+1
-            VyS = x[iVy-1]
-            VyN = NeumannVerticalVelocity(σ_BC, x[iVy-1], rheo.G[end], rheo.K[end], τyy0[end], P0[end], Δy, Δt)
+            VyS = x[iy-1]
+            # VyN = 2*V_BC.y.N - x[iy-1] 
+            VyN = NeumannVerticalVelocity(σ_BC, x[iy-1], rheo.G[end], rheo.K[end], τyy0[end], P0[end], Δy, Δt)
         else
-            VyS = x[iVy-1]
-            VyN = x[iVy]
+            VyS = x[iy-1]
+            VyN = x[iy]
         end
         divV  =  (VyN-VyS)/Δy
         F[iP] = -(x[iP] - P0[i]) - divV*rheo.K[i]*Δt
@@ -157,6 +187,7 @@ function main(σ0)
         #---------------#
         K   = 10e6,#6.6666666667e6, # K = 3/2*Gv in Vermeer (1990)
         G   = 10e6,
+        lc  = 0.01,  
         ν   = 0., 
         c   = 0.0e4,
         ϕ   = 40/180*π,
@@ -195,11 +226,11 @@ function main(σ0)
             y = convert(Array, CSV.read("./data/Vermeer1990_Friction_CaseB.csv", DataFrame, header =[:x, :y])[:,2]),
         )
     end
-    y    = (min=0.0/sc.L, max=1.0/sc.L)
+    y    = (min=-0.5/sc.L, max=0.5/sc.L)
 
     V_BC = ( 
         x = ( 
-            S = 0.0,
+            S = ε̇0*y.min,
             N = ε̇0*y.max,
         ),
         y = (
@@ -208,56 +239,65 @@ function main(σ0)
         )
     )
   
-    Ncy = 20
-    Nt  = params.nt * Int16(Ncy/10)
+    Ncy = 30
+    Nt  = params.nt
     Δy  = (y.max-y.min)/Ncy
-    Δt  = params.Δt/sc.t * 10/Ncy
-    yc  = LinRange(-y.min+Δy/2, y.max-Δy/2, Ncy  )
-    yv  = LinRange(-y.min,      y.max,      Ncy+1)
+    Δt  = params.Δt/sc.t
+    yc  = LinRange(y.min+Δy/2, y.max-Δy/2, Ncy  )
+    yv  = LinRange(y.min,      y.max,      Ncy+1)
 
     rheo = (
         c    = params.c/sc.σ*ones(Ncy+1).+250/sc.σ,
-        # c    = params.c/sc.σ*ones(Ncy+1).+0.0000001/sc.σ,
+        # c    = params.c/sc.σ*ones(Ncy+1).+0.001/sc.σ,
         # c    = params.c/sc.σ*ones(Ncy+1),
-        ψ    = params.ψ     *ones(Ncy+1),
-        ϕ    = params.ϕ     *ones(Ncy+1),
-        ν    = params.ν     *ones(Ncy+1),
-        G    = params.G/sc.σ*ones(Ncy+1),
-        K    = params.K/sc.σ*ones(Ncy+1),
-        ηvp  = params.ηvp   *ones(Ncy+1)/(sc.σ*sc.t),
+        ψ    = params.ψ      *ones(Ncy+1),
+        ϕ    = params.ϕ      *ones(Ncy+1),
+        ν    = params.ν      *ones(Ncy+1),
+        G    = params.G/sc.σ *ones(Ncy+1),
+        K    = params.K/sc.σ *ones(Ncy+1),
+        ηvp  = params.ηvp    *ones(Ncy+1)/(sc.σ*sc.t),
+        lc   = params.lc/sc.L*ones(Ncy+1),
     )
 
-    rheo.c[Int64(floor(Ncy/2)+1)] = params.c/sc.σ
+    rheo.c[Int64(ceil(Ncy/2)+1)] = params.c/sc.σ
     # @. rheo.K = 2/3 .* rheo.G.*(1 .+ rheo.ν) ./ (1 .- 2 .* rheo.ν)
+    # rheo.ϕ[Int64(floor(Ncy/2))] *= 0.99999999
     τxx  = τxxi*ones(Ncy+1)
     τyy  = τyyi*ones(Ncy+1)
     τzz  = τzzi*ones(Ncy+1)
     τxy  = τxyi*ones(Ncy+1)
     P    =   Pi*ones(Ncy+1)
+    Rz   =    zeros(Ncy+1)
+    myz  =    zeros(Ncy+1)
     fc   = zeros(Ncy+1)
     pl   = zeros(Ncy+1)
     Pc   = zeros(Ncy+1)
-    # rheo.G[Int64(floor(Ncy/2))] = rheo.G[Int64(floor(Ncy/2))]/3
-    # P[Int64(floor(Ncy/2))] = P[Int64(floor(Ncy/2))]*1.00001
+    
+    # rheo.G[1:Int64(floor(Ncy/2))] .*= 2.1
+    # P[Int64(floor(Ncy/2))] = P[Int64(floor(Ncy/2))]*1.1
+    # rheo.G[Int64(floor(Ncy/2))] /= 10
     # rheo.c[end] = 1000
 
     Vx   = collect(ε̇0.*yc)
     Vy   = zeros(Ncy+0)
     Vx0  = collect(ε̇0.*yc)
     Vy0  = zeros(Ncy+0)
+    ω̇z   =    zeros(Ncy+0)
     ρ    = params.ρ/(sc.L*sc.σ*sc.t^2) * ones(Ncy+0)
 
     τxx0 = zeros(Ncy+1)
     τyy0 = zeros(Ncy+1)
     τzz0 = zeros(Ncy+1)
     τxy0 = zeros(Ncy+1)
+    Rz0  =    zeros(Ncy+1)
+    myz0 =    zeros(Ncy+1)
     εyyt = zeros(Ncy+1)
     P0   = zeros(Ncy+1)
 
-    N    = 2*(Ncy+0) + Ncy+1
+    N    = 3*(Ncy+0) + Ncy+1
     F    = zeros(N)
     x    = zeros(N)
-    iV   = (x=Ncy, y=2*(Ncy))  
+    ind   = (x=Ncy, y=2*(Ncy), p=2*(Ncy)+(Ncy+1))  
     NumV = (x=1:Ncy, y=Ncy+1:2*Ncy)
     probes = (
         fric = zeros(Nt),
@@ -275,7 +315,7 @@ function main(σ0)
     # Sparsity pattern
     input       = rand(N)
     output      = similar(input)
-    Res_closed! = (F,x) -> Res!(F, x, P, P0, τxx0, τyy0, τzz0, τxy0, ρ, Vx0, Vy0, V_BC, σ_BC, rheo, iV, NumV, Δy, Δt )
+    Res_closed! = (F,x) -> Res!(F, x, P, P0, τxx0, τyy0, τzz0, τxy0, Rz0, myz0, ρ, Vx0, Vy0, V_BC, σ_BC, rheo, ind, NumV, Δy, Δt )
     sparsity    = Symbolics.jacobian_sparsity(Res_closed!, output, input)
     J           = Float64.(sparse(sparsity))
 
@@ -300,14 +340,17 @@ function main(σ0)
         P0   .= P
         Vx0  .= Vx 
         Vy0  .= Vy
+        myz0 .= myz
+        Rz0  .= Rz
 
         # Vx  .= collect(ε̇0.*yc) 
         # Vy  .= 0*Vy
 
         # Populate global solution array
-        x[1:iV.x]      .= Vx
-        x[iV.x+1:iV.y] .= Vy
-        x[iV.y+1:end]  .= P
+        x[1:ind.x]       .= Vx
+        x[ind.x+1:ind.y] .= Vy
+        x[ind.y+1:ind.p] .= P
+        x[ind.p+1:end  ] .= ω̇z
 
         ϵglob = 1e-13
 
@@ -315,7 +358,7 @@ function main(σ0)
         for iter=1:100
 
             # Residual
-            Res!(F, x, P, P0, τxx0, τyy0, τzz0, τxy0, ρ, Vx0, Vy0, V_BC, σ_BC, rheo, iV, NumV, Δy, Δt )
+            Res!(F, x, P, P0, τxx0, τyy0, τzz0, τxy0, Rz0, myz0, ρ, Vx0, Vy0, V_BC, σ_BC, rheo, ind, NumV, Δy, Δt )
             @show (iter, norm(F))
             if norm(F)/length(F)<ϵglob break end
 
@@ -333,12 +376,13 @@ function main(σ0)
         if norm(F)/length(F)>ϵglob error("Diverged!") end
 
         # Extract fields from global solution array
-        Vx .= x[1:iV.x]
-        Vy .= x[iV.x+1:iV.y]
-        P  .= x[iV.y+1:end]
+        Vx .= x[1:ind.x]
+        Vy .= x[ind.x+1:ind.y]
+        P  .= x[ind.y+1:ind.p]
+        ω̇z .= x[ind.p+1:end  ]
 
         # Compute stress for postprocessing
-        ComputeStress!(τxx, τyy, τzz, τxy, Pc, fc, pl, εyyt, x, P, P0, τxx0, τyy0, τzz0, τxy0, V_BC, σ_BC, rheo, iV, NumV, Δy, Δt )
+        ComputeStress!(τxx, τyy, τzz, τxy, Pc, Rz, myz, fc, pl, εyyt, x, P, P0, τxx0, τyy0, τzz0, τxy0, Rz0, myz0, V_BC, σ_BC, rheo, ind, NumV, Δy, Δt )
         P .= Pc #!!!!!!!!!
 
         # Postprocessing
@@ -350,14 +394,15 @@ function main(σ0)
         maps.P[it,:]   .= P
 
         nout = 1000
-        if mod(it, nout)==0
+        if mod(it, nout)==0 || it==1
             # p1 = plot()
             # p1 = plot!(Vx, yc, label="Vx")
             # p1 = plot!(Vy, yc, label="Vy")
             # p1 = plot!(P,  yv, label="P")
             p1 = heatmap((1:Nt)*ε̇0*Δt*100, yc, maps.Vx[1:Nt,:]', title="Vx", xlabel="strain", ylabel="y") #, clim=(0,1)
-            p2 = heatmap((1:Nt)*ε̇0*Δt*100, yc, maps.Vy[1:Nt,:]', title="Vy", xlabel="strain", ylabel="y") #, clim=(0,1/2)
+            # p2 = heatmap((1:Nt)*ε̇0*Δt*100, yc, maps.Vy[1:Nt,:]', title="Vy", xlabel="strain", ylabel="y") #, clim=(0,1/2)
             # p3 = heatmap((1:Nt)*ε̇0*Δt*100, yv, maps.P[1:Nt,:]',  title="P",  xlabel="strain", ylabel="y")
+            p2 = plot(ω̇z, yc)
             p3 = scatter(V90.x, V90.y)
             p3 = plot!((1:it)*ε̇0*Δt*100, probes.fric[1:it])
             # p4 = plot((1:it)*ε̇0*Δt*100, probes.εyy[1:it]*100)
